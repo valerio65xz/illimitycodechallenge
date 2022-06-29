c61aaf367ee2cb61ce5ff44f8fd6a5b9a18fded1
vale.bond <vale-bond@hotmail.it>
2022-06-29 16:05:44 +0200

    deleted useless message

--- src/main/java/com/illimity/codechallenge/exception/ResponseException.java
+++ src/main/java/com/illimity/codechallenge/exception/ResponseException.java
@@ -3,20 +3,13 @@ package com.illimity.codechallenge.exception;
 public class ResponseException extends RuntimeException {
 
     private final ResponseErrorEnum error;
-    private final String message;
 
     public ResponseException(ResponseErrorEnum error) {
         this.error = error;
-        this.message = null;
     }
 
     public ResponseErrorEnum getError() {
         return error;
-    }
-
-    @Override
-    public String getMessage() {
-        return this.message;
     }
 
 }
